57655f5db6689842713f24fbcc0671ece0957b4f
Andrejs Maklakovs <101195894+andreymaklakov@users.noreply.github.com>
2023-04-15 01:05:13 +0300

    Update confiig.ts

--- src/confiig.ts
+++ src/confiig.ts
@@ -1,1 +1,2 @@
-export const API_URL = "http://localhost:3001/customers";
+// export const API_URL = "http://localhost:3001/customers";
+export const API_URL = "https://functional-flame-zinnia.glitch.me/customers"
